3df1087eacbab88e7765af4006c82270750f1f47
tetsu05 <tetsu051955@gmail.com>
2021-09-14 14:07:31 +0000

    [Middle]21

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,6 +16,16 @@ class BooksController < ApplicationController
     end
 
     def edit
+      @book = Book.find(params[:id])
+    end
+    
+    def update
+    end
+    
+    def destroy
+      book = Book.find(params[:id])
+      book.destroy
+      redirect_to books_path
     end
     
     private
