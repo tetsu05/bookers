6003d68c4128a9ff9ae1bcccb930a36ef4df471c
tetsu05 <tetsu051955@gmail.com>
2021-09-15 07:47:09 +0000

    [Middle]27

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,8 +11,15 @@ class BooksController < ApplicationController
 
     def create
       book = Book.new(book_params)
-      book.save
-      redirect_to book_path(book)
+      @book = Book.new
+      @books = Book.all
+      if book.save
+        flash[:notice] = "Book was successfully created."
+        redirect_to book_path(book.id)
+      else
+        render :index
+      end
+
     end
 
     def edit
@@ -22,6 +29,7 @@ class BooksController < ApplicationController
     def update
       book = Book.find(params[:id])
       book.update(book_params)
+      flash[:notice] = "Book was successfully update."
       redirect_to book_path(book.id)
     end
 
